375ff2e7bcd4dbb83fd934c09736bd1721cd920f
Uffe <uffed1978@gmail.com>
2025-02-27 22:13:49 +0200

    render updates

--- aibrewer/backend/app.py
+++ aibrewer/backend/app.py
@@ -42,4 +42,5 @@ def serve_static(path):
     return send_from_directory('../frontend', path)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(host='0.0.0.0', port=port)
